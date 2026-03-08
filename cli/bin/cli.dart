e0d6b9346d240f67d157cc022eae37413efc4a00
Krisha Shilpakar <krishashilpakar8@gmail.com>
2026-03-08 21:22:25 +0545

    Moved .git folder to dartpedia root to track entire project and Added new package 'command_runner' for modular command handling

---
+++ cli/bin/cli.dart
@@ -0,0 +1,8 @@
+// import 'dart:io';
+// import 'package:http/http.dart' as http;
+import 'package:command_runner/command_runner.dart';
+
+void main(List<String> arguments) async {
+  var runner = CommandRunner();
+  await runner.run(arguments);
+}
